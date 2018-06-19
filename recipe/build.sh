7479526c1891681c3a37550e2f9e7e87c49b0d2b
Min RK <benjaminrk@gmail.com>
2018-06-19 14:33:39 +0200

    set mpi lib names directly for mpich + macos
    workaround cmake bug 1952

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-export CC=mpicc
-export CXX=mpicxx
-export FC=mpifort
+if [[ $(uname) == "Darwin" && $mpi == "mpich" ]]; then
+  # workaround cmake bug https://gitlab.kitware.com/cmake/cmake/merge_requests/1952
+  # remove when we require cmake >= 3.12
+  EXTRA_CMAKE="-DMPI_C_LIB_NAMES=mpi;pmpi -DMPI_CXX_LIB_NAMES=mpi;pmpi;mpicxx"
+fi
 
 mkdir build && cd build
 cmake \
+    $EXTRA_CMAKE \
     -DBUILD_SHARED_LIBS=ON \
     -DCMAKE_INSTALL_PREFIX="$PREFIX" \
     -DCMAKE_PREFIX_PATH="$PREFIX" \
