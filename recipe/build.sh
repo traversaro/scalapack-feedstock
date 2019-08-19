a5bf8f64bdac77df55d441aeb89987628e0cf0b9
Isuru Fernando <isuruf@gmail.com>
2019-08-19 18:12:12 -0500

    Fix for gfortran 4

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
-if [[ $(uname) == "Darwin" && $mpi == "mpich" ]]; then
-  # workaround cmake bug https://gitlab.kitware.com/cmake/cmake/merge_requests/1952
-  # remove when we require cmake >= 3.12
-  EXTRA_CMAKE="-DMPI_C_LIB_NAMES=mpi;pmpi -DMPI_CXX_LIB_NAMES=mpi;pmpi;mpicxx"
-fi
-
-if [[ "$c_compiler" == "gcc" ]]; then
-  # seems to be needed to find libquadmath during detection with conda gcc
-  export LD_LIBRARY_PATH=$PREFIX/lib
+if [[ "$target_platform" == "osx-64" ]]; then
+  TOOLS_DIR=$(dirname $($FC --print-libgcc-file-name))
+  if [[ ! -f "$TOOLS_DIR/ld" ]]; then
+    ln -sf $LD $TOOLS_DIR/ld
+  fi
 fi
 
 mkdir build && cd build
@@ -17,9 +13,7 @@ cmake \
     -DBUILD_SHARED_LIBS=ON \
     -DCMAKE_INSTALL_PREFIX="$PREFIX" \
     -DCMAKE_PREFIX_PATH="$PREFIX" \
+    -DCMAKE_BUILD_TYPE=Release \
     .. || (cat CMakeFiles/CMakeOutput.log && cat CMakeFiles/CMakeError.log && exit 1)
-
-# unset after detection
-unset LD_LIBRARY_PATH
 
 make install -j${CPU_COUNT}
