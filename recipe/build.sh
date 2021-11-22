df3522c1d2792b05cbf5d50d4ae8d2b234e53a2f
Julien Schueller <schueller@phimeca.com>
2021-11-22 13:37:34 +0100

    Enable osx/arm

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,10 +9,9 @@ if [[ "$target_platform" == "osx-64" ]]; then
 fi
 
 mkdir build && cd build
-cmake \
+cmake ${CMAKE_ARGS} \
     $EXTRA_CMAKE \
     -DBUILD_SHARED_LIBS=ON \
-    -DCMAKE_INSTALL_PREFIX="$PREFIX" \
     -DCMAKE_PREFIX_PATH="$PREFIX" \
     -DCMAKE_BUILD_TYPE=Release \
     .. || (cat CMakeFiles/CMakeOutput.log && cat CMakeFiles/CMakeError.log && exit 1)
