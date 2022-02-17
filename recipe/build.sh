ed177ba6f357c2e3ec1120f282d0b4c0927b801e
Tobias Fischer <info@tobiasfischer.info>
2022-02-18 07:43:15 +1000

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,12 +9,14 @@ if [[ "$target_platform" == "osx-64" ]]; then
 fi
 
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
-  EXTRA_CMAKE="-DCDEFS=Add_ -DMPI_C_LIB_NAMES=$PREFIX/lib/libmpi$SHLIB_EXT -DMPI_C_LIBRARIES=$PREFIX/lib/libmpi$SHLIB_EXT -DMPI_C_ADDITIONAL_INCLUDE_DIRS=$PREFIX/include -DMPI_C=$PREFIX/lib/libmpi$SHLIB_EXT -DMPI_C_WORKS=TRUE -DMPI_Fortran_LIB_NAMES=$PREFIX/lib/libmpifort$SHLIB_EXT -DMPI_Fortran_LIBRARIES=$PREFIX/lib/libmpifort$SHLIB_EXT -DMPI_Fortran_ADDITIONAL_INCLUDE_DIRS=$PREFIX/include -DMPI_Fortran=$PREFIX/lib/libmpifort$SHLIB_EXT -DMPI_Fortran_WORKS=TRUE -DMPI_DETERMINE_LIBRARY_VERSION=FALSE -DMPI_C_VERSION=3.1 -DMPI_Fortran_VERSION=3.1"
+  # EXTRA_CMAKE="-DCDEFS=Add_ -DMPI_C_LIB_NAMES=$PREFIX/lib/libmpi$SHLIB_EXT -DMPI_C_LIBRARIES=$PREFIX/lib/libmpi$SHLIB_EXT -DMPI_C_ADDITIONAL_INCLUDE_DIRS=$PREFIX/include -DMPI_C=$PREFIX/lib/libmpi$SHLIB_EXT -DMPI_C_WORKS=TRUE -DMPI_Fortran_LIB_NAMES=$PREFIX/lib/libmpifort$SHLIB_EXT -DMPI_Fortran_LIBRARIES=$PREFIX/lib/libmpifort$SHLIB_EXT -DMPI_Fortran_ADDITIONAL_INCLUDE_DIRS=$PREFIX/include -DMPI_Fortran=$PREFIX/lib/libmpifort$SHLIB_EXT -DMPI_Fortran_WORKS=TRUE -DMPI_DETERMINE_LIBRARY_VERSION=FALSE -DMPI_C_VERSION=3.1 -DMPI_Fortran_VERSION=3.1"
   # https://github.com/Reference-ScaLAPACK/scalapack/issues/21
+  EXTRA_CMAKE="-DCDEFS=Add_ "
   export FFLAGS="${FFLAGS} -fallow-argument-mismatch"
   export OPAL_PREFIX=$PREFIX
   export CC=mpicc
   export CXX=mpic++
+  export FC=mpiifort
 fi
 
 mkdir build && cd build
